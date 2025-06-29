e5541f5483da52bb41dfde2f768c3e010749162b
Zhanibek-Khan <zhanibek.amankul.almaty@gmail.com>
2025-06-29 15:08:12 +0500

    Update google_auth_helper.py

--- google_auth_helper.py
+++ google_auth_helper.py
@@ -1,12 +1,12 @@
 import json
 from google.oauth2 import service_account
 
-def get_credentials():
+def get_credentials(scopes):
     credentials_path = "/etc/secrets/credentials.json"
     with open(credentials_path, "r") as f:
         service_account_info = json.load(f)
     credentials = service_account.Credentials.from_service_account_info(
         service_account_info,
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+        scopes=scopes
     )
     return credentials
