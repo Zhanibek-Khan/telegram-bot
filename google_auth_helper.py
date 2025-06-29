b1161ec6fc1eba5628fde0bdc0b2562a638d7305
Zhanibek-Khan <zhanibek.amankul.almaty@gmail.com>
2025-06-29 14:39:37 +0500

    update google_auth_helper to fix credential loading

--- google_auth_helper.py
+++ google_auth_helper.py
@@ -1,16 +1,12 @@
 import os
 import json
-from google.oauth2.service_account import Credentials
+from google.oauth2 import service_account
 
-def get_credentials(scopes):
-    # Получаем JSON из переменной окружения
-    json_str = os.environ.get("GOOGLE_CREDENTIALS_JSON")
-    if not json_str:
-        raise Exception("Сервисный аккаунт не найден в переменных окружения GOOGLE_CREDENTIALS_JSON!")
-
-    # Преобразуем JSON-строку в словарь
-    info = json.loads(json_str)
-
-    # Создаем credentials с указанными скоупами
-    creds = Credentials.from_service_account_info(info, scopes=scopes)
-    return creds
+def get_google_credentials():
+    credentials_path = "/etc/secrets/credentials.json"
+    with open(credentials_path) as f:
+        service_account_info = json.load(f)
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_info, scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+    return credentials
